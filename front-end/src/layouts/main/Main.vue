46ad6a7bb34e73554288f00277026677fb4701bf
cristiantorres91 <49564966+cristiantorres91@users.noreply.github.com>
2021-02-20 12:25:28 -0600

    Rutas Buscar y Comprar

--- front-end/src/layouts/main/Main.vue
+++ front-end/src/layouts/main/Main.vue
@@ -166,37 +166,38 @@ export default {
       navMenuItems      : navMenuItems,
       routerTransition  : themeConfig.routerTransition || 'none',
       routeTitle        : this.$route.meta.pageTitle,
-      steps: [{
-          target  : '#btnVNavMenuMinToggler',
-          content : 'Toggle Collapse Sidebar.'
-        },
-        {
-          target  : '.vx-navbar__starred-pages',
-          content : 'Create your own bookmarks. You can also re-arrange them using drag & drop.'
-        },
-        {
-          target  : '.i18n-locale',
-          content : 'You can change language from here.'
-        },
-        {
-          target  : '.navbar-fuzzy-search',
-          content : 'Try fuzzy search to visit pages in flash.'
-        },
-        {
-          target  : '.customizer-btn',
-          content : 'Customize template based on your preference',
-          params  : {
-            placement: 'left'
-          }
-        },
-        {
-          target  : '.vs-button.buy-now',
-          content : 'Buy this awesomeness at affordable price!',
-          params  : {
-            placement: 'top'
-          }
-        },
-      ],
+      // steps: [
+      //   {
+      //     target  : '#btnVNavMenuMinToggler',
+      //     content : 'Toggle Collapse Sidebar.'
+      //   },
+      //   {
+      //     target  : '.vx-navbar__starred-pages',
+      //     content : 'Create your own bookmarks. You can also re-arrange them using drag & drop.'
+      //   },
+      //   {
+      //     target  : '.i18n-locale',
+      //     content : 'You can change language from here.'
+      //   },
+      //   {
+      //     target  : '.navbar-fuzzy-search',
+      //     content : 'Try fuzzy search to visit pages in flash.'
+      //   },
+      //   {
+      //     target  : '.customizer-btn',
+      //     content : 'Customize template based on your preference',
+      //     params  : {
+      //       placement: 'left'
+      //     }
+      //   },
+      //   {
+      //     target  : '.vs-button.buy-now',
+      //     content : 'Buy this awesomeness at affordable price!',
+      //     params  : {
+      //       placement: 'top'
+      //     }
+      //   },
+      // ],
     }
   },
   watch: {
