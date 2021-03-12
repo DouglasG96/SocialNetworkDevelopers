e9bb7cb5c49bc5dad3b88ef1ee04b3ec4e98e1eb
Cristian Torres <ADALBERTO108@hotmail.com>
2021-03-12 16:22:36 -0600

    Deslogue Funcionando(borro  token y redirije a login)

--- front-end/src/layouts/MainLayout.vue
+++ front-end/src/layouts/MainLayout.vue
@@ -13,34 +13,62 @@
         <q-toolbar-title>
           SND
         </q-toolbar-title>
-        <q-space/>
+        <q-space />
         <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn round dense flat color="white" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
-                 @click="$q.fullscreen.toggle()"
-                 v-if="$q.screen.gt.sm">
+          <q-btn
+            round
+            dense
+            flat
+            color="white"
+            :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+            @click="$q.fullscreen.toggle()"
+            v-if="$q.screen.gt.sm"
+          >
           </q-btn>
-          <q-btn round dense flat color="white" icon="fab fa-github" type="a" href="https://github.com/pratik227/quasar-admin" target="_blank">
+          <q-btn
+            round
+            dense
+            flat
+            color="white"
+            icon="fab fa-github"
+            type="a"
+            href="https://github.com/pratik227/quasar-admin"
+            target="_blank"
+          >
           </q-btn>
-          <q-btn round dense flat icon="fas fa-heart" style="color:#9d4182 !important;" type="a" href="https://github.com/sponsors/pratik227" target="_blank">
+          <q-btn
+            round
+            dense
+            flat
+            icon="fas fa-heart"
+            style="color:#9d4182 !important;"
+            type="a"
+            href="https://github.com/sponsors/pratik227"
+            target="_blank"
+          >
           </q-btn>
           <q-btn round dense flat color="white" icon="notifications">
             <q-badge color="red" text-color="white" floating>
               5
             </q-badge>
-            <q-menu
-            >
+            <q-menu>
               <q-list style="min-width: 100px">
                 <messages></messages>
                 <q-card class="text-center no-shadow no-border">
-                  <q-btn label="View All" style="max-width: 120px !important;" flat dense
-                         class="text-indigo-8"></q-btn>
+                  <q-btn
+                    label="View All"
+                    style="max-width: 120px !important;"
+                    flat
+                    dense
+                    class="text-indigo-8"
+                  ></q-btn>
                 </q-card>
               </q-list>
             </q-menu>
           </q-btn>
           <q-btn round flat>
             <q-avatar size="26px">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
             </q-avatar>
           </q-btn>
           <q-btn round flat v-if="isLoggedIn" @click="logout()">
@@ -56,17 +84,17 @@
       bordered
       content-class="bg-primary text-white"
     >
-    <q-list>
+      <q-list>
         <q-item to="/" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
-            <q-icon name="shopping_cart"/>
+            <q-icon name="shopping_cart" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Product Catalogues</q-item-label>
           </q-item-section>
         </q-item>
-    </q-list>
-    
+      </q-list>
+
       <!-- <q-list>
         <q-item to="/" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
@@ -299,44 +327,46 @@
     </q-drawer>
 
     <q-page-container class="bg-grey-2">
-      <router-view/>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+import EssentialLink from "components/EssentialLink";
 import Messages from "./Messages";
 import { mapActions } from "vuex";
 
-    export default {
-        name: 'MainLayout',
+export default {
+  name: "MainLayout",
 
-        components: {
-            Messages,
-            EssentialLink
-        },
+  components: {
+    Messages,
+    EssentialLink
+  },
 
-        data() {
-            return {
-                leftDrawerOpen: false,
-            }
-        },
-    computed : {
-      isLoggedIn : function(){ return this.$store.getters['auth/isLoggedIn']}
-    },
-        methods: {
-          ...mapActions("auth", ["logout"]),
-          async logout() {
-            console.log('logout');
-            try {
-              const resp = await this.logout();
-              console.log(resp);
-              await this.$router.push({path: '/Login'}).catch(error => { });
-            } catch (error) {
-              console.log(error);
-            }
-          },
-        },
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  },
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters["auth/isLoggedIn"];
     }
+  },
+  methods: {
+    ...mapActions("auth", ["logoutUsers"]),
+    async logout() {
+      try {
+        console.log("logout");
+        const resp = await this.logoutUsers();
+        console.log(resp);
+        await this.$router.push({ path: "/Login" }).catch(error => {});
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
 </script>
