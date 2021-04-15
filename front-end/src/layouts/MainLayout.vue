db302c96c2fac2c5ab4fc9e20b648aab1b070ee6
Cristian Torres <ADALBERTO108@hotmail.com>
2021-04-15 17:03:27 -0600

    seccion vendedor menu

--- front-end/src/layouts/MainLayout.vue
+++ front-end/src/layouts/MainLayout.vue
@@ -118,6 +118,21 @@
             </q-item>
           </q-list>
         </q-expansion-item>
+        <q-expansion-item icon="person" label="Vendedor">
+          <q-list class="q-pl-lg">
+            <q-item
+              to="/Mantenimientos/Usuarios"
+              active-class="q-item-no-link-highlighting"
+            >
+              <q-item-section avatar>
+                <q-icon name="shopping_bag" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Publicar</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
       </q-list>
 
       <!-- <q-list>
