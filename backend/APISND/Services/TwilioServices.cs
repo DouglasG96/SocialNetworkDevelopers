cfc0cacc5745db789e7f181bb76bf2c40a452ebe
Alice2015@ <cristiantorresalfaro91@gmail.com>
2021-04-25 16:25:14 -0600

    Se agrega servicio Twilio

---
+++ backend/APISND/Services/TwilioServices.cs
@@ -0,0 +1,62 @@
+﻿using APISND.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace APISND.Services
+{
+    public class TwilioServices : ITwilio
+    {
+        private readonly string _accountSid = "ACcedeee9dbd2d2a12959337b3404742a9";
+        private readonly string _authToken = "5dee42cc1277c09bbc21d1104eca0e80";
+        public bool SendSMS(string number, string msg)
+        {
+            try
+            {
+                TwilioClient.Init(_accountSid, _authToken);
+
+                var messageOptions = new CreateMessageOptions(
+                    new PhoneNumber(string.Format("+503{0}", number)));
+                messageOptions.MessagingServiceSid = "MG648440611ba9866d251e08319f4fff70";
+                messageOptions.Body = msg;
+
+                var message = MessageResource.Create(messageOptions);
+                Console.WriteLine(message.Body);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public bool SendWhatsApp(string number, string msg)
+        {
+            try
+            {
+                TwilioClient.Init(_accountSid, _authToken);
+
+                var messageOptions = new CreateMessageOptions(
+                    new PhoneNumber(string.Format("whatsapp:+503{0}", number)));
+                messageOptions.From = new PhoneNumber("whatsapp:+14155238886");
+                messageOptions.Body = msg;
+
+                var message = MessageResource.Create(messageOptions);
+                Console.WriteLine(message.Body);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+}
