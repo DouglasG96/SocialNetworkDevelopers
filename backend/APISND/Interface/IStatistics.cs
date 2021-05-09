05a7ae5f9a1b18dfdafabe0c8e1e1a498cdb898e
Douglas Guzman <drgb96@gmail.com>
2021-05-08 19:42:59 -0500

    Creacion de clases para estadisticas

---
+++ backend/APISND/Interface/IStatistics.cs
@@ -0,0 +1,14 @@
+﻿using APISND.Models;
+using APISND.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APISND.Interface
+{
+    public interface IStatistics
+    {
+        List<StatisticsDTO> GetStatistics();
+    }
+}
